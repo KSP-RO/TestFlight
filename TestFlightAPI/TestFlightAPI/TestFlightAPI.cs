70985c79ed7d3d7dec62a2b258f349ee766207f2
John Vanderbeck <jwvanderbeck@gmail.com>
2014-12-28 17:52:21 -0800

    Documenting the API

--- TestFlightAPI/TestFlightAPI/TestFlightAPI.cs
+++ TestFlightAPI/TestFlightAPI/TestFlightAPI.cs
@@ -5,43 +5,106 @@ namespace TestFlightAPI
 {
 	public struct TestFlightData
 	{
+        // Scope is a combination of the current SOI and the Situation, always lowercase.
+        // EG "kerbin_atmosphere" or "mun_space"
+        // The one exception is "deep-space" which applies regardless of the SOI if you are deep enough into space
 		public string scope;
+        // The total accumulated flight data for the part
 		public float flightData;
+        // The specific flight time, in seconds, of this part instance
 		public int flightTime;
 	}
 
 	public struct TestFlightFailureDetails
 	{
-        // Failure details
+        // Human friendly title to display in the MSD for the failure.  25 characters max
         public string failureTitle;
+        // "minor", "failure", or "major" used to indicate the severity of the failure to the player
 		public string severity;
+        // chances of the failure occuring relative to other failure modules on the same part
+        // This should never be anything except:
+        // 2 = Rare, 4 = Seldom, 8 = Average, 16 = Often, 32 = Common
 		public int weight;
+        // "mechanical" indicates a physical failure that requires physical repair
+        // "software" indicates a software or electric failure that might be fixed remotely by code
 		public string failureType;
-        public bool canBeRepaired;  // Indicates in broad sense, is it ever possible to attempt a repair?
+        // Indicates in broad sense, is it ever possible to attempt a repair under some condition?
+        public bool canBeRepaired;
         // Repair details
+        // Time required to complete repairs.  Not currently supported.
         public int repairTimeRequired;
         // For mechanical failures
+        // Does the repair require that a Kerbal be EVA to repair the part?
         public bool requiresEVA;
+        // Can the part be repaired while the vessel is in flight?
         public bool canBeRepairedInFlight;
+        // Can the part be repaired when the vessel is landed on the surface?
         public bool canBeRepairedOnLanded;
+        // Can the part be repaired when the vessel is splashed down in the water?
         public bool canBeRepairedOnSplashed;
+        // How many spare parts does it take to attempt repair?
         public int sparePartsRequired;
         // For software failures
+        // Can the part be repaired by remote?
         public bool canBeRepairedByRemote;
 	}
 
 	public interface IFlightDataRecorder
 	{
+        /// <summary>
+        /// Called frequently by TestFlightCore to ask the module for the current flight data.
+        /// The module should only return the currently active scope's data, and should return the most up
+        /// to date data it has.
+        /// This method should only RETURN the current flight data, not calculate it.  Calculation
+        /// should be done in DoFlightUpdate()
+        /// </summary>
+        /// <returns>The current flight data.</returns>
 		TestFlightData GetCurrentFlightData();
+
+        /// <summary>
+        /// Initializes the flight data on a newly instanced part from the stored persistent flight data.
+        /// This data should only be accepted the first time ever.
+        /// </summary>
+        /// <param name="allFlightData">A list of all TestFlightData stored for the part, once for each known scope</param>
         void InitializeFlightData(List<TestFlightData> allFlightData);
+
+        /// <summary>
+        /// Called to set what is considered "deep-space" altitude
+        /// </summary>
+        /// <param name="newThreshold">New threshold.</param>
         void SetDeepSpaceThreshold(double newThreshold);
+
+        /// <summary>
+        /// Called frequently by TestFlightCore to let the DataRecorder do an update cycle to calulate the current flight data.
+        /// This is where the calculation of current data based on paremeters (such as elapsed MET) should occur.
+        /// Generally this will be called immediately prior to GetcurrentFlightData() so that the DataRecorder
+        /// can be up to date.
+        /// </summary>
+        /// <param name="missionStartTime">Mission start time in seconds.</param>
+        /// <param name="flightDataMultiplier">Global Flight data multiplier.  A user setting which should modify the internal collection rate.  Amount of collected data should be multiplied against this.  Base is 1.0 IE no modification.</param>
+        /// <param name="flightDataEngineerMultiplier">Flight data engineer multiplier.  A user setting mutiplier that makes the engineer bonus more or less.  1.0 is base.</param>
         void DoFlightUpdate(double missionStartTime, double flightDataMultiplier, double flightDataEngineerMultiplier);
+
+        /// <summary>
+        /// Returns the current data situation, "atmosphere", "space", or "deep-space"
+        /// </summary>
+        /// <returns>The Situation of the current data scope</returns>
         string GetDataSituation();
+
+        /// <summary>
+        /// Returns current SOI
+        /// </summary>
+        /// <returns>The current SOI for the data scope</returns>
         string GetDataBody();
 	}
 
 	public interface ITestFlightReliability
 	{
+        /// <summary>
+        /// Gets the current reliability of the part as calculated based on the given flightData
+        /// </summary>
+        /// <returns>The current reliability.  Can be negative in order to reduce overall reliability from other Reliability modules.</returns>
+        /// <param name="flightData">Flight data on which to calculate reliability.</param>
         float GetCurrentReliability(TestFlightData flightData);
 	}
 
