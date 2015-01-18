9f39848dae8bbb3b6c37d6371135c528243fcbc7
John Vanderbeck <jwvanderbeck@gmail.com>
2015-01-18 08:32:59 -0800

    Still working on initial editor window

--- TestFlightCore/TestFlightCore/TestFlightEditor.cs
+++ TestFlightCore/TestFlightCore/TestFlightEditor.cs
@@ -21,6 +21,9 @@ namespace TestFlightCore
         internal bool Visible;
         internal TestFlightManagerScenario tfScenario = null;
         internal bool isReady = false;
+        private ApplicationLauncherButton appLauncherButton;
+        bool stickyWindow = false;
+
 
         internal override void Start()
         {
@@ -48,15 +51,78 @@ namespace TestFlightCore
         internal void Startup()
         {
             CalculateWindowBounds();
+            StartCoroutine("AddToToolbar");
             isReady = true;
         }
 
         internal void CalculateWindowBounds()
         {
-            if (showGraph)
-                WindowRect = new Rect(Screen.width - 550, Screen.height - 550, 500, 500);
-            else
-                WindowRect = new Rect(Screen.width - 550, Screen.height - 300, 500, 250);
+            WindowRect = new Rect(Screen.width - 575, Screen.height - 250, 350, 200);
+        }
+
+        IEnumerator AddToToolbar()
+        {
+            while (!ApplicationLauncher.Ready)
+            {
+                yield return null;
+            }
+            try
+            {
+                // Load the icon for the button
+                Debug.Log("TestFlight MasterStatusDisplay: Loading icon texture");
+                Texture iconTexture = GameDatabase.Instance.GetTexture("TestFlight/Resources/AppLauncherIcon", false);
+                if (iconTexture == null)
+                {
+                    throw new Exception("TestFlight MasterStatusDisplay: Failed to load icon texture");
+                }
+                Debug.Log("TestFlight MasterStatusDisplay: Creating icon on toolbar");
+                appLauncherButton = ApplicationLauncher.Instance.AddModApplication(
+                    OpenWindow,
+                    CloseWindow,
+                    HoverInButton,
+                    HoverOutButton,
+                    null,
+                    null,
+                    ApplicationLauncher.AppScenes.VAB | ApplicationLauncher.AppScenes.SPH,
+                    iconTexture);
+                ApplicationLauncher.Instance.AddOnHideCallback(HideButton);
+                ApplicationLauncher.Instance.AddOnRepositionCallback(RepostionWindow);
+            }
+            catch (Exception e)
+            {
+                Debug.Log("TestFlight MasterStatusDisplay: Unable to add button to application launcher: " + e.Message);
+                throw e;
+            }
+        }
+        void OpenWindow()
+        {
+            CalculateWindowBounds();
+            Visible = true;
+            stickyWindow = true;
+        }
+        void CloseWindow()
+        {
+            Visible = false;
+            stickyWindow = false;
+        }
+        void HideButton()
+        {
+            ApplicationLauncher.Instance.RemoveModApplication(appLauncherButton);
+        }
+        void RepostionWindow()
+        {
+            CalculateWindowBounds();
+            Debug.Log("TestFlight MasterStatusDisplay: RepositionWindow");
+        }
+        void HoverInButton()
+        {
+            CalculateWindowBounds();
+            Visible = true;
+        }
+        void HoverOutButton()
+        {
+            if (!stickyWindow)
+                Visible = false;
         }
 
         internal override void OnGUIOnceOnly()
@@ -75,52 +141,71 @@ namespace TestFlightCore
             {
                 ITestFlightCore core = null;
                 GUILayout.BeginArea(WindowRect, SkinsLibrary.CurrentSkin.GetStyle("HUD"));
-                GUILayout.BeginVertical();
-                GUILayout.Label(String.Format("Selected Part: {0}", selectedPart.name));
-                tfScenario.settings.editorShowGraph = DrawToggle(ref tfScenario.settings.editorShowGraph, "Show Reliability Graph", Styles.styleToggle);
-                tfScenario.settings.editorShowOnDemand = DrawToggle(ref tfScenario.settings.editorShowOnDemand, "Show Window on Demand", Styles.styleToggle);
-
-                tfScenario.settings.currentEditorScrollPosition = GUILayout.BeginScrollView(tfScenario.settings.currentEditorScrollPosition);
-                PartFlightData partData = tfScenario.GetFlightDataForPartName(selectedPart.name);
-                if (partData != null)
+                if (selectedPart != null)
                 {
-                    foreach (PartModule pm in selectedPart.Modules)
+                    GUILayout.BeginVertical();
+                    GUILayout.Label(String.Format("Selected Part: {0}", selectedPart.name));
+
+                    tfScenario.settings.currentEditorScrollPosition = GUILayout.BeginScrollView(tfScenario.settings.currentEditorScrollPosition);
+                    PartFlightData partData = tfScenario.GetFlightDataForPartName(selectedPart.name);
+                    if (partData != null)
                     {
-                        core = pm as ITestFlightCore;
-                        if (core != null)
+                        foreach (PartModule pm in selectedPart.Modules)
                         {
-                            break;
+                            core = pm as ITestFlightCore;
+                            if (core != null)
+                            {
+                                break;
+                            }
+                        }
+                        if (core != null && showGraph)
+                        {
+                            List<TestFlightData> flightData = partData.GetFlightData();
+                            core.InitializeFlightData(flightData, 1.0);
+                            foreach (TestFlightData data in flightData)
+                            {
+                                GUILayout.BeginHorizontal();
+                                double failureRate = core.GetBaseFailureRateForScope(data.scope);
+                                String mtbfString = core.FailureRateToMTBFString(failureRate, TestFlightUtil.MTBFUnits.SECONDS, 999);
+                                // 10 characters for body max plus 10 characters for situation plus underscore = 21 characters needed for longest scope string
+                                GUILayout.Label(core.PrettyStringForScope(data.scope), GUILayout.Width(125));
+                                GUILayout.Label(String.Format("{0,-7:F2}<b>du</b>", data.flightData), GUILayout.Width(75));
+                                //TODO this needs to change to be MTBF once the new system goes in
+//                                GUILayout.Label(String.Format("{0,-5:F2} MTBF", mtbfString), GUILayout.Width(125));
+                                GUILayout.Label(String.Format("{0,-5:F2}<b>%R</b>", failureRate), GUILayout.Width(125));
+                                FloatCurve curve = null;
+                                curve = core.GetBaseReliabilityCurveForScope(data.scope);
+                                if (curve != null)
+                                {
+                                    if (GUILayout.Button("G", GUILayout.Width(38)))
+                                    {
+                                        // Display graph for scope
+                                        Vector2 startPoint = new Vector2(5, 200);
+                                        Vector2 endPoint = new Vector2(100, 500);
+                                        Color lineColor = XKCDColors.Yellow;
+                                        float start = curve.minTime;
+                                        float end = curve.maxTime;
+                                        // fit the curve
+                                        Drawing.DrawLine(startPoint, endPoint, lineColor, 5f);
+                                    }
+                                }
+                                GUILayout.EndHorizontal();
+                            }
                         }
                     }
-                    if (core != null)
+                    else
                     {
-                        List<TestFlightData> flightData = partData.GetFlightData();
-                        core.InitializeFlightData(flightData, 1.0);
-                        foreach (TestFlightData data in flightData)
-                        {
-                            GUILayout.BeginHorizontal();
-                            double failureRate = core.GetBaseFailureRateForScope(data.scope);
-                            String mtbfString = core.FailureRateToMTBFString(failureRate, TestFlightUtil.MTBFUnits.SECONDS, 999);
-                            // 10 characters for body max plus 10 characters for situation plus underscore = 21 characters needed for longest scope string
-                            GUILayout.Label(data.scope, GUILayout.Width(150));
-                            GUILayout.Label(String.Format("{0,-7:F2}<b>du</b>", data.flightData), GUILayout.Width(75));
-                            GUILayout.Label(String.Format("{0,-5:F2} MTBF", mtbfString), GUILayout.Width(150));
-                            GUILayout.EndHorizontal();
-                        }
+                        GUILayout.Label("No flight data has been recorded for this part.");
                     }
+                    GUILayout.EndScrollView();
+                    GUILayout.EndVertical();
                 }
                 else
                 {
-                    GUILayout.Label("No flight data has been recorded for this part.");
+                    GUILayout.BeginVertical();
+                    GUILayout.Label("This window will show accumulated flight data and failure rate details for a part when you mouse over it in the parts bin.");
+                    GUILayout.EndVertical();
                 }
-                GUILayout.EndScrollView();
-
-                GUILayout.Space(250);
-                GUILayout.EndVertical();
-                Vector2 startPoint = new Vector2(5, 200);
-                Vector2 endPoint = new Vector2(100, 500);
-                Color lineColor = XKCDColors.Yellow;
-                Drawing.DrawLine(startPoint, endPoint, lineColor, 5f);
                 GUILayout.EndArea();
             }
         }
@@ -160,17 +245,30 @@ namespace TestFlightCore
         {
             switch (ptr.evt)
             {
+                case POINTER_INFO.INPUT_EVENT.PRESS:
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        // Left button press
+                        Debug.Log("LEFT");
+                    }
+                    else if (Input.GetMouseButtonDown(1))
+                    {
+                        // Right button press
+                        Debug.Log("RIGHT");
+                    }
+                    break;
                 case POINTER_INFO.INPUT_EVENT.MOVE:
                     if (!mouseFlag)
                     {
                         mouseFlag = true;
                         TestFlightEditorWindow.Instance.selectedPart = selectedPart.partPrefab;
-                        TestFlightEditorWindow.Instance.Visible = true;
+//                        TestFlightEditorWindow.Instance.Visible = true;
                     }
                     break;
                 case POINTER_INFO.INPUT_EVENT.MOVE_OFF:
                     mouseFlag = false;
-                    TestFlightEditorWindow.Instance.Visible = false;
+                    TestFlightEditorWindow.Instance.selectedPart = null;
+//                    TestFlightEditorWindow.Instance.Visible = false;
                     break;
             }
         }
