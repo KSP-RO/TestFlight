081b0249a1d373e5d643c9b275bd306ba82287b1
John Vanderbeck <jwvanderbeck@gmail.com>
2015-01-18 08:33:16 -0800

    New settings file for body aliases

---
+++ TestFlightCore/TestFlightCore/BodySettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using KSP;
+using UnityEngine;
+using KSPPluginFramework;
+
+namespace TestFlightCore
+{
+    public class BodySettings : ConfigNodeStorage
+    {
+        public BodySettings(String FilePath) : base(FilePath) {
+
+        }
+
+        [Persistent] public Dictionary<String, String> bodyAliases = new Dictionary<string, string>();
+    }
+}
+
