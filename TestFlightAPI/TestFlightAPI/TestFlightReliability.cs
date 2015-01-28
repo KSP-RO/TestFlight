df4130f4e4108b68d14c0ef8ac7df18c4a7dff05
John Vanderbeck <jwvanderbeck@gmail.com>
2015-01-28 11:22:11 -0800

    removed double failure check

--- TestFlightAPI/TestFlightAPI/TestFlightReliability.cs
+++ TestFlightAPI/TestFlightAPI/TestFlightReliability.cs
@@ -340,7 +340,8 @@ namespace TestFlightAPI
             double survivalChance = reliability / lastReliability;
             lastReliability = reliability;
             Debug.Log(String.Format("TestFlightReliability: Survival Chance at Time {0:F2} is {1:f4}", (float)operatingTime, survivalChance));
-            float failureRoll = Mathf.Min(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+//            float failureRoll = Mathf.Min(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+            float failureRoll = UnityEngine.Random.value;
             if (failureRoll > survivalChance)
             {
 //                Debug.Log(String.Format("TestFlightReliability: Survival Chance at Time {0:F2} is {1:f4} -- {2:f4}^({3:f4}*{0:f2}*-1.0)", (float)operatingTime, survivalChance, Mathf.Exp(1), (float)currentFailureRate));
