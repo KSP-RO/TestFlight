c1b26e3eaa1e01e8b546a1738bdf526d3d7493b2
John Vanderbeck <jwvanderbeck@gmail.com>
2015-01-28 09:12:25 -0800

    JSON -> MM Config setup
    Python script will compile JSON based TestFlight configs into expanded
    and valid ModuleManage Configs for all parts specified.
    Added initial test config for Stock liquid engines.

---
+++ configs/compileConfigs.py
@@ -0,0 +1,133 @@
+# This utility script compiles TestFlight configurations into final proper ModuleManager config files
+
+import os, argparse, sys, json
+
+ALL_BODIES = [
+	"sun",
+	"moho",
+	"eve",
+	"gilly",
+	"kerbin",
+	"min",
+	"minmus",
+	"duna",
+	"ike",
+	"dres"
+	"jool",
+	"laythe",
+	"vall",
+	"tylo",
+	"bop",
+	"pol",
+	"eeloo"
+]
+
+ALL_SITUATIONS = [
+	"atmosphere",
+	"space"
+]
+
+ALL_SCOPES = [
+	"deep_space"
+]
+
+class DefaultHelpParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+
+def loadJSON(jsonFilename):
+    with open(jsonFilename, "r") as jsonFile:
+        rawJson = json.load(jsonFile)
+        return rawJson
+
+def compileFailureDef(d):
+	definition = rawJson["FailureConfigs"][d]
+	node = ""
+	for key in definition:
+		node += "\n\t\t{} = {}".format(key, definition[key])
+	return node
+
+def compileRepairDef(d):
+	definition = rawJson["RepairConfigs"][d]
+	node = "\n\t\tREPAIR\n\t\t{"
+	for key in definition:
+		node += "\n\t\t\t{} = {}".format(key, definition[key])
+	node += "\n\t\t}"
+	return node
+
+def compileCurveDef(d):
+	definition = rawJson["ReliabilityDefinitions"][d]
+	# build reliability curves for each scope unless its overridden
+	configNodes = ""
+	for scope in ALL_SCOPES:
+		configNodes += "\n\t\tRELIABILITY_BODY\n\t\t{"
+		configNodes += "\n\t\t\tscope = " + scope
+		configNodes += "\n\t\t\treliabilityCurve"
+		configNodes += "\n\t\t\t{"
+		curve = definition["default"]
+		if scope in definition:
+			curve = definition[scope]
+		for key in curve:
+			configNodes += "\n\t\t\t\t" + key
+		configNodes += "\n\t\t\t}"
+		configNodes += "\n\t\t}"
+	return configNodes
+
+def compile(pattern, config):
+	node = pattern + "\n"
+	node += "{\n\tMODULE\n\t{\n\t\tname = TestFlightCore\n\t}\n"
+
+	configDef = rawJson["TestFlightConfigs"][config]
+	for moduleName in configDef:
+		moduleConfig = configDef[moduleName]
+		node += "\n\tMODULE\n\t{\n\t\tname = " + moduleName + "\n\t}"
+		for key in moduleConfig:
+			if key == "CURVE_DEF":
+				node += compileCurveDef(moduleConfig[key])
+			elif key == "REPAIR_DEF":
+				node += compileRepairDef(moduleConfig[key])
+			elif key == "FAILURE_DEF":
+				node += compileFailureDef(moduleConfig[key])
+			else:
+				if isinstance(moduleConfig[key], dict):
+					node += "\n\t\t" + key
+					node += "\n\t\t{"
+					for subKey in moduleConfig[key]["keys"]:
+						node += "\n\t\t\t" + subKey
+					node += "\n\t\t}"
+				else:
+					node += "\n\t\t{} = {}".format(key, moduleConfig[key])
+	node += "\n}"
+	return node
+
+
+HELP_DESC = "Compiles a given TestFlight config.json to standard ModuleManager.cfg"
+parser = DefaultHelpParser(description=HELP_DESC)
+parser.add_argument('config', metavar='file', type=str, nargs=1,
+                   help='config.json file to compile')
+
+args = parser.parse_args()
+
+print args
+
+if not args.config or len(args.config) < 1:
+    print "ERROR: No configuration file specified.  Configuration file must be specified."
+    sys.exit(2)
+
+rawJson = loadJSON(args.config[0])
+
+# Init scopes
+for body in ALL_BODIES:
+	for situation in ALL_SITUATIONS:
+		ALL_SCOPES.append(body + "_" + situation)
+
+# Process each defined part config
+for partConfig in rawJson["PartConfigs"].values():
+	config = partConfig["config"]
+	patterns = partConfig["patterns"]
+	for pattern in patterns:
+		print compile(pattern, config)
+
+
