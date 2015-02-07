f785317a0592195d3b04a1bdaff48e131c06efed
John Vanderbeck <jwvanderbeck@gmail.com>
2015-02-07 15:05:53 -0800

    Update build system to no longer care about Stock
    All release from here on out will be RO only.

--- makeMeta.py
+++ makeMeta.py
@@ -9,30 +9,23 @@ class DefaultHelpParser(argparse.ArgumentParser):
 HELP_DESC = "Creates neccesary metadata files"
 parser = DefaultHelpParser(description=HELP_DESC)
 parser.add_argument('tag', metavar='tag', type=str, nargs=1,
-                   help='tag of release (e.g. 0.4.6.0-RealismOverhaul')
+                   help='tag of release (e.g. 0.4.6.0')
 
 args = parser.parse_args()
 
 if not args.tag or len(args.tag) < 1:
-    print "ERROR: git tag must be specified and must be in the format major.minor.patch.build-configuration.\ne.g. 0.4.6.0-RealismOverhaul"
+    print "ERROR: git tag must be specified and must be in the format major.minor.patch.build-configuration.e.g. 0.4.6.0"
     sys.exit(2)
 
-version = args.tag[0].split("-")[0]
-configuration = args.tag[0].split("-")[1]
-if configuration == "Stock":
-	opposingConfiguration = "RealismOverhaul"
-	avcID = 117
-elif configuration == "RealismOverhaul":
-	opposingConfiguration = "Stock"
-	avcID = 118
+version = args.tag[0]
 major = int(version.split(".")[0])
 minor = int(version.split(".")[1])
 patch = int(version.split(".")[2])
 build = int(version.split(".")[3])
 # create AVC .version file
 avc = {
-	"NAME" : "TestFlight-{}".format(configuration),
-	"URL" : "http://ksp-avc.cybutek.net/version.php?id={}".format(avcID),
+	"NAME" : "TestFlight",
+	"URL" : "http://ksp-avc.cybutek.net/version.php?id=118",
 	"DOWNLOAD" : "https://github.com/jwvanderbeck/TestFlight/releases/download/{}/TestFlight-{}.zip".format(args.tag[0],args.tag[0]),
 	"CHANGE_LOG_URL" : "https://github.com/jwvanderbeck/TestFlight/releases/tag/{}".format(args.tag[0]),
 	"VERSION" :
@@ -52,34 +45,3 @@ avc = {
 with open("TestFlight.version", "w") as f:
 	f.write(json.dumps(avc))
 
-# create CKAN'T file
-ckant = {
-	"spec_version" : 1,
-	"name" : "TestFlight-{}".format(configuration),
-	"abstract" : "Persistent part research & reliability system gives you a reason to test fly your space hardware.  Fly parts to gain data, the more data the more reliable the parts.",
-	"identifier" : "TestFlight-{}".format("configuration"),
-	"download" : "https://github.com/jwvanderbeck/TestFlight/releases/download/{}/TestFlight-{}.zip".format(args.tag[0],args.tag[0]),
-	"license" : "CC-BY-NC-SA-4.0",
-	"version" : version,
-	"release_status" : "testing",
-	"ksp_version" : "0.90",
-	"resources" : {
-		"homepage" : "http://forum.kerbalspaceprogram.com/threads/88187",
-		"repository" : "https://github.com/jwvanderbeck/TestFlight",
-		"bugtracker" : "https://github.com/jwvanderbeck/TestFlight/issues",
-	},
-	"install" : [
-        {
-            "file"       : "GameData/TestFlight",
-            "install_to" : "GameData/TestFlight"
-        }
-    ],
-	"depends" : [
-        { "name" : "ModuleManager", "min_version" : "2.3.8" }
-    ],
-    "conflicts" : [
-    	{ "name" : "TestFlight-{}".format(opposingConfiguration)}
-    ]
-}
-with open("TestFlight-{}-{}.ckan".format(configuration, version), "w") as f:
-	f.write(json.dumps(ckant,sort_keys=True,indent=4, separators=(',', ': ')))
--- makefile
+++ makefile
@@ -4,34 +4,14 @@ CONFIG_DIR = configs
 VERSION = $(shell git describe --tags)
 BRANCH := $(shell git rev-parse --abbrev-ref HEAD 2>&1)
 
-ifdef TRAVIS_TAG
-BUILD := $(shell echo $(TRAVIS_TAG) | cut -d - -f 2)
-else
-BUILD := $(BRANCH)
-endif
-
 ZIPFILE := $(PROJ_NAME)-$(TRAVIS_TAG).zip
-
-ro: clean_all clean_RealismOverhaul configs_RealismOverhaul
-	cp -r GameData/TestFlight/ ~/Dropbox/KSP/TestFlight/
 
 all: configs
 
 release: zip
-	echo BUILD IS $(BUILD)
 
-configs_master: configs_HEAD
-
-configs_HEAD: configs_Stock configs_RealismOverhaul
-
-configs_Stock: $(CONFIG_DIR)/Stock/%.cfg
-	cp $(CONFIG_DIR)/Stock/*.cfg GameData/TestFlight
-
-configs_RealismOverhaul: $(CONFIG_DIR)/RealismOverhaul/%.cfg
+configs: $(CONFIG_DIR)/RealismOverhaul/%.cfg
 	cp $(CONFIG_DIR)/RealismOverhaul/*.cfg GameData/TestFlight
-
-$(CONFIG_DIR)/Stock/%.cfg:
-	cd $(CONFIG_DIR);python compileConfigs.py Stock
 
 $(CONFIG_DIR)/RealismOverhaul/%.cfg:
 	cd $(CONFIG_DIR);python compileConfigs.py RealismOverhaul
@@ -40,33 +20,15 @@ ifdef TRAVIS_TAG
 meta:
 	python makeMeta.py $(TRAVIS_TAG)
 	cp TestFlight.version GameData/TestFlight/TestFlight.version
-	cp *.ckan GameData/TestFlight
 else
 meta:
 endif
 
-zip: configs_$(BUILD) meta
+zip: configs meta
 	zip -r $(ZIPFILE) GameData
 
-clean: clean_$(BUILD)
-	-rm GameData/TestFlight/*.cfg
-	-rm *.zip
-	-rm GameData/TestFlight/*.version
-	-rm GameData/TestFlight/*.ckan
-	-rm *.version
-	-rm *.ckan
-
-clean_master: clean_HEAD
-
-clean_HEAD: clean_Stock clean_RealismOverhaul
-
-clean_Stock:
-	-rm $(CONFIG_DIR)/Stock/*.cfg
-
-clean_RealismOverhaul:
+clean:
 	-rm $(CONFIG_DIR)/RealismOverhaul/*.cfg
-
-clean_all:
 	-rm GameData/TestFlight/*.cfg
 	-rm *.zip
 	-rm GameData/TestFlight/*.version
