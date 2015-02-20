1781eae8fc031eb78015afce8391adff7f4256aa
John Vanderbeck <jwvanderbeck@gmail.com>
2015-02-20 10:27:01 -0800

    IgnitonFail uses new engine module addressing

--- TestFlightFailure_IgnitionFail.cs
+++ TestFlightFailure_IgnitionFail.cs
@@ -13,13 +13,23 @@ namespace TestFlight
     {
         [KSPField(isPersistant=true)]
         public bool restoreIgnitionCharge = false;
+        [KSPField(isPersistant=true)]
+        public string engineIndex = "";
+        [KSPField(isPersistant=true)]
+        public string engineID = "";
 
         public FloatCurve ignitionFailureRate;
-        public int engineIndex = 0;
 
         private ITestFlightCore core = null;
-        private EngineModuleWrapper.EngineIgnitionState previousIgnitionState = EngineModuleWrapper.EngineIgnitionState.UNKNOWN;
-        private EngineModuleWrapper engine = null;
+
+        protected struct EngineHandler
+        {
+            public EngineModuleWrapper.EngineIgnitionState ignitionState;
+            public EngineModuleWrapper engine;
+            public bool failEngine;
+        }
+
+        List<EngineHandler> engines = null;
 
         public new bool TestFlightEnabled
         {
@@ -32,7 +42,7 @@ namespace TestFlight
                 if (ignitionFailureRate == null)
                     return false;
                 // and a valid engine
-                if (engine == null)
+                if (engines == null)
                     return false;
 
                 return TestFlightUtil.EvaluateQuery(Configuration, this.part);
@@ -70,10 +80,96 @@ namespace TestFlight
                 if (modulePrefab != null && modulePrefab.Configuration == configuration)
                     ignitionFailureRate = modulePrefab.ignitionFailureRate;
             }
-            if (engineIndex > 0)
-                engine = new EngineModuleWrapper(this.part, engineIndex);
+            engines = new List<EngineHandler>();
+            if (!String.IsNullOrEmpty(engineID))
+            {
+                if (engineID.ToLower() == "all")
+                {
+                    List<ModuleEnginesFX> enginesFX = this.part.Modules.OfType<ModuleEnginesFX>().ToList();
+                    foreach (ModuleEnginesFX fx in enginesFX)
+                    {
+                        string id = fx.engineID;
+                        EngineModuleWrapper engine = new EngineModuleWrapper(this.part, id);
+                        EngineHandler engineHandler = new EngineHandler();
+                        engineHandler.engine = engine;
+                        engineHandler.ignitionState = EngineModuleWrapper.EngineIgnitionState.UNKNOWN;
+                        engines.Add(engineHandler);
+                    }
+                }
+                else if (engineID.Contains(","))
+                {
+                    string[] sEngineIndices = engineID.Split(new char[1] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string sEngineIndex in sEngineIndices)
+                    {
+                        EngineModuleWrapper engine = new EngineModuleWrapper(this.part, sEngineIndex);
+                        EngineHandler engineHandler = new EngineHandler();
+                        engineHandler.engine = engine;
+                        engineHandler.ignitionState = EngineModuleWrapper.EngineIgnitionState.UNKNOWN;
+                        engines.Add(engineHandler);
+                    }
+                }
+                else
+                {
+                    int index = int.Parse(engineIndex);
+                    EngineModuleWrapper engine = new EngineModuleWrapper(this.part, index);
+                    EngineHandler engineHandler = new EngineHandler();
+                    engineHandler.engine = engine;
+                    engineHandler.ignitionState = EngineModuleWrapper.EngineIgnitionState.UNKNOWN;
+                    engines.Add(engineHandler);
+                }
+            }
+            else if (!String.IsNullOrEmpty(engineIndex))
+            {
+                if (engineIndex.ToLower() == "all")
+                {
+                    int length = this.part.Modules.Count;
+                    for (int i = 0; i < length; i++)
+                    {
+                        PartModule pm = this.part.Modules.GetModule(i);
+                        ModuleEngines pmEngine = pm as ModuleEngines;
+                        ModuleEnginesFX pmEngineFX = pm as ModuleEnginesFX;
+                        EngineModuleWrapper engine = null;
+                        if (pmEngine != null || pmEngineFX != null)
+                        {
+                            engine = new EngineModuleWrapper(this.part, i);
+                            EngineHandler engineHandler = new EngineHandler();
+                            engineHandler.engine = engine;
+                            engineHandler.ignitionState = EngineModuleWrapper.EngineIgnitionState.UNKNOWN;
+                            engines.Add(engineHandler);
+                        }
+                    }
+                }
+                else if (engineIndex.Contains(","))
+                {
+                    string[] sEngineIndices = engineIndex.Split(new char[1] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string sEngineIndex in sEngineIndices)
+                    {
+                        int index = int.Parse(sEngineIndex);
+                        EngineModuleWrapper engine = new EngineModuleWrapper(this.part, index);
+                        EngineHandler engineHandler = new EngineHandler();
+                        engineHandler.engine = engine;
+                        engineHandler.ignitionState = EngineModuleWrapper.EngineIgnitionState.UNKNOWN;
+                        engines.Add(engineHandler);
+                    }
+                }
+                else
+                {
+                    int index = int.Parse(engineIndex);
+                    EngineModuleWrapper engine = new EngineModuleWrapper(this.part, index);
+                    EngineHandler engineHandler = new EngineHandler();
+                    engineHandler.engine = engine;
+                    engineHandler.ignitionState = EngineModuleWrapper.EngineIgnitionState.UNKNOWN;
+                    engines.Add(engineHandler);
+                }
+            }
             else
-                engine = new EngineModuleWrapper(this.part);
+            {
+                EngineModuleWrapper engine = new EngineModuleWrapper(this.part);
+                EngineHandler engineHandler = new EngineHandler();
+                engineHandler.engine = engine;
+                engineHandler.ignitionState = EngineModuleWrapper.EngineIgnitionState.UNKNOWN;
+                engines.Add(engineHandler);
+            }
         }
 
         public override void OnUpdate()
@@ -81,34 +177,39 @@ namespace TestFlight
             if (!TestFlightEnabled)
                 return;
 
-            EngineModuleWrapper.EngineIgnitionState currentIgnitionState = engine.IgnitionState;
-            // If we are transitioning from not ignited to ignited, we do our check
-            // The ignitionFailueRate defines the failure rate per flight data
-
-            if (currentIgnitionState == EngineModuleWrapper.EngineIgnitionState.IGNITED)
+            // For each engine we are tracking, compare its current ignition state to our last known ignition state
+            for (int i = 0; i < engines.Count; i++)
             {
-                if (previousIgnitionState == EngineModuleWrapper.EngineIgnitionState.NOT_IGNITED || previousIgnitionState == EngineModuleWrapper.EngineIgnitionState.UNKNOWN)
+                EngineHandler engine = engines[i];
+                EngineModuleWrapper.EngineIgnitionState currentIgnitionState = engine.engine.IgnitionState;
+                // If we are transitioning from not ignited to ignited, we do our check
+                // The ignitionFailueRate defines the failure rate per flight data
+
+                if (currentIgnitionState == EngineModuleWrapper.EngineIgnitionState.IGNITED)
                 {
-                    // We want the initial flight data, not the current here
-                    double initialFlightData = core.GetInitialFlightData();
-                    double failureRate = ignitionFailureRate.Evaluate((float)initialFlightData);
-                    failureRate = Mathf.Max((float)failureRate, (float)TestFlightUtil.MIN_FAILURE_RATE);
-
-                    // r1 = the chance of survival right now at time index 1
-                    // r2 = the chance of survival through ignition and into initial run up
-
-                    double r1 = Mathf.Exp((float)-failureRate * 1f);
-                    double r2 = Mathf.Exp((float)-failureRate * 3f);
-                    double survivalChance = r2 / r1;
-                    double failureRoll = core.RandomGenerator.NextDouble();
-                    if (failureRoll > survivalChance)
+                    if (engine.ignitionState == EngineModuleWrapper.EngineIgnitionState.NOT_IGNITED || engine.ignitionState == EngineModuleWrapper.EngineIgnitionState.UNKNOWN)
                     {
-                        core.TriggerNamedFailure("TestFlightFailure_IgnitionFail");
+                        // We want the initial flight data, not the current here
+                        double initialFlightData = core.GetInitialFlightData();
+                        double failureRate = ignitionFailureRate.Evaluate((float)initialFlightData);
+                        failureRate = Mathf.Max((float)failureRate, (float)TestFlightUtil.MIN_FAILURE_RATE);
+
+                        // r1 = the chance of survival right now at time index 1
+                        // r2 = the chance of survival through ignition and into initial run up
+
+                        double r1 = Mathf.Exp((float)-failureRate * 1f);
+                        double r2 = Mathf.Exp((float)-failureRate * 3f);
+                        double survivalChance = r2 / r1;
+                        double failureRoll = core.RandomGenerator.NextDouble();
+                        if (failureRoll > survivalChance)
+                        {
+                            engine.failEngine = true;
+                            core.TriggerNamedFailure("TestFlightFailure_IgnitionFail");
+                        }
                     }
                 }
+                engine.ignitionState = currentIgnitionState;
             }
-
-            previousIgnitionState = currentIgnitionState;
         }
 
         // Failure methods
@@ -117,17 +218,33 @@ namespace TestFlight
             base.DoFailure();
             if (!TestFlightEnabled)
                 return;
+            for (int i = 0; i < engines.Count; i++)
+            {
+                EngineHandler engine = engines[i];
+                if (engine.failEngine)
+                {
+                    engine.engine.Shutdown();
+                    if (OneShot && restoreIgnitionCharge)
+                        RestoreIgnitor();
+                    engine.failEngine = false;
+                }
+            }
 
-            engine.Shutdown();
-
-            if (OneShot && restoreIgnitionCharge)
-                RestoreIgnitor();
         }
         public override double DoRepair()
         {
             base.DoRepair();
-            if (restoreIgnitionCharge)
-                RestoreIgnitor();
+            for (int i = 0; i < engines.Count; i++)
+            {
+                EngineHandler engine = engines[i];
+                if (engine.failEngine)
+                {
+                    engine.engine.Shutdown();
+                    if (restoreIgnitionCharge)
+                        RestoreIgnitor();
+                    engine.failEngine = false;
+                }
+            }
             return 0;
         }
         public void RestoreIgnitor()
@@ -150,10 +267,6 @@ namespace TestFlight
             }
             else
                 ignitionFailureRate = null;
-            if (node.HasValue("engineIndex"))
-                engineIndex = int.Parse(node.GetValue("engineIndex"));
-            else
-                engineIndex = 0;
         }
     }
 }
