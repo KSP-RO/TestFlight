744b84b169c3522580c1b02a65fdbedd11bc9297
anxcon <okthissux@hotmail.com>
2016-03-07 20:40:49 -0800

    * **FIX** Reworked pressure reliability

---
+++ Reliability Modules/TestFlightReliability_DynamicPressure.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TestFlightAPI;
+
+namespace TestFlight
+{
+    public class TestFlightReliability_DynamicPressure : TestFlightReliabilityBase
+    {
+        [KSPField]
+        public FloatCurve reliabilityAtPressure = new FloatCurve();
+
+        private double oldPenalty = 1.0;
+        public override void OnUpdate()
+        {
+            if (!TestFlightEnabled)
+                return;
+
+            if (core == null)
+                return;
+
+            double newPenalty = reliabilityAtPressure.Evaluate((float)base.vessel.dynamicPressurekPa * 1000);
+            if (newPenalty < 1d) newPenalty = 1d;
+            if (newPenalty != this.oldPenalty)
+            {
+                this.oldPenalty = newPenalty;
+                core.SetTriggerMomentaryFailureModifier("DynamicPressure", newPenalty, this);
+            }
+        }
+        public override double GetBaseFailureRate(float flightData)
+        {
+            return 0;
+        }
+    }
+}
