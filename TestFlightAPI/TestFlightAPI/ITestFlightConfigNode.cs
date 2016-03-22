68787aaa2b3b441a248ad1599e0eac1133057383
John Vanderbeck <jwvanderbeck@gmail.com>
2016-03-22 11:42:56 -0700

    New classes for handling multiple core configs - wip probably will be removed or refactored

---
+++ TestFlightAPI/TestFlightAPI/ITestFlightConfigNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestFlightAPI
+{
+    public interface ITestFlightConfigNode
+    {
+        string PartFilter { get; set; }
+
+        string PartName { get; set; }
+
+        string PartTitle { get; set; }
+
+        bool PartEnabled { get; set; }
+    }
+}
+
