0e62761de1d3ab10fa29faf4f77f7387f9f6ab65
John Vanderbeck <jwvanderbeck@gmail.com>
2016-04-03 14:42:41 -0700

    Updates to KSPPluginFraeWork to work with new KSP 1.1 GUI Changes

--- TestFlightCore/TestFlightCore/FrameworkExt/MonoBehaviourWindowPlus.cs
+++ TestFlightCore/TestFlightCore/FrameworkExt/MonoBehaviourWindowPlus.cs
@@ -149,6 +149,7 @@ namespace KSPPluginFramework
         }
         internal override void OnGUIEvery()
         {
+            base.OnGUIEvery();
             ddlManager.CloseOnOutsideClicks();
         }
 
