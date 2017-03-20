3918081b781e55c5f3a5d752fce9b7c8dff7d4bb
John Vanderbeck <jwvanderbeck@gmail.com>
2017-03-20 12:18:24 -0700

    * **FIX**: TestFlightHUD tries to access an invalid MasterStatusItem if CurrentVessel isn't tracked, or vessel change occurs. Should fix #155

--- TestFlightCore/TestFlightCore/TestFlightHUD.cs
+++ TestFlightCore/TestFlightCore/TestFlightHUD.cs
@@ -86,6 +86,9 @@ namespace TestFlightCore
 
             // Display information on active vessel
             Guid currentVessl = FlightGlobals.ActiveVessel.id;
+            if (!masterStatus.ContainsKey(currentVessl))
+                return;
+            
 //            if (masterStatus[currentVessl].allPartsStatus.Count(ps => ps.activeFailure != null) < lastPartCount)
             if (masterStatus[currentVessl].allPartsStatus.Count < lastPartCount)
             {
