ba54b94b0e2ee1a6603d44e8e1e3ad530604bffd
DRVeyl <DRVeyl@gmail.com>
2022-02-13 11:05:09 -0500

    Bump AssemblyInfo for v2.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
 
 [assembly: KSPAssembly("TestFlight", 2, 1)]
-[assembly: KSPAssemblyDependency("TestFlightAPI", 1, 5)]
+[assembly: KSPAssemblyDependency("TestFlightAPI", 2, 1)]
 
--- TestFlightCore/TestFlightCore/Properties/AssemblyInfo.cs
+++ TestFlightCore/TestFlightCore/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.CompilerServices;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
@@ -26,5 +26,5 @@ using System.Runtime.CompilerServices;
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
-[assembly: KSPAssemblyDependency("TestFlightAPI", 1, 5)]
+[assembly: KSPAssemblyDependency("TestFlightAPI", 2, 1)]
 
